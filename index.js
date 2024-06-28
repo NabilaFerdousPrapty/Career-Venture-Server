dd65f0a4fa963ee628357265a41e590fa46b2f50
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-28 23:21:46 +0600

    errors fixed

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors');
 
+app.use(cors(
+  {
+      origin: ["http://localhost:5173",
+         
+      ],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+  }
+))
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
